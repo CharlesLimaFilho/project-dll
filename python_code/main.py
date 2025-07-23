0fb0c1fd059666a6a19082ebba6dad3b2deb65b7
Charles Lima <charles.lima.filho17@gmail.com>
2025-07-23 01:37:59 -0300

    ## Initial commit

---
+++ python_code/main.py
@@ -0,0 +1,15 @@
+import ctypes
+import os
+import sys
+
+# Carrega a biblioteca dinâmica
+#lib_path = os.path.join("..", "c_code", "libjogador.dylib")
+lib = ctypes.CDLL('../c_code/jogador.dll')
+
+# Configura a função descreve_jogador
+lib.descreve_jogador.argtypes = [ctypes.c_char_p]
+lib.descreve_jogador.restype = None
+
+nome = "Lucero"
+nome_bytes = nome.encode('utf-8')
+lib.descreve_jogador(nome_bytes)
